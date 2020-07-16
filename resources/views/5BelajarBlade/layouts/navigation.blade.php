abe396c37a2055934077032553c68ff4c4087a32
Abi Malik <abimalikr@gmail.com>
2020-07-16 15:11:04 +0700

    Rev Blade Function

--- resources/views/5BelajarBlade/layouts/navigation.blade.php
+++ resources/views/5BelajarBlade/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <a href="5BelajarBlade.home">Home</a>
+    <a href="/">Home</a>
     <a href="5BelajarBlade.contact">Contact</a>
     <a href="5BelajarBlade.about">About</a>
     <a href="5BelajarBlade.login">Login</a>
